09a6aaa33540b7de39611ab45fc213efea896a06
Henry Kuo <mhhenryss@gmail.com>
2019-10-21 14:03:59 +0800

    add example

--- CSharpGeneric/Program.cs
+++ CSharpGeneric/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,8 +11,32 @@ namespace CSharpGeneric
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hellow Word!");
+            TestArrayList();
+        }
+
+        private static void TestArrayList()
+        {
+            var arrayList = new ArrayList();
+
+            foreach (var item in arrayList)
+            {
+                Console.WriteLine(item);
+            }
+
             Console.ReadLine();
         }
+
+        //private static void TestList()
+        //{
+        //    var ints = new List<int>();
+
+        //    foreach (var i in ints)
+        //    {
+        //        Console.WriteLine(i);
+        //    }
+
+        //    Console.ReadLine();
+        //}
+
     }
 }
