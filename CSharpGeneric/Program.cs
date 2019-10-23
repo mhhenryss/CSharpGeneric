05b002eb292b8118644ec34e3390586ecb39a5f8
Henry Kuo <mhhenryss@gmail.com>
2019-10-23 10:03:16 +0800

    add example

--- CSharpGeneric/Program.cs
+++ CSharpGeneric/Program.cs
@@ -21,73 +21,13 @@ namespace CSharpGeneric
     {
         static void Main(string[] args)
         {
-            var a = new Customer()
-            {
-                Name = "Big Winner",
-                Balance = 99999999
-            };
+            var a = 1;
+            var b = 2;
 
-            var b = new Customer()
-            {
-                Name = "Loser",
-                Balance = -10000
-            };
+            Console.WriteLine($"a:{a}");
+            Console.WriteLine($"b:{b}");
 
-            //var a = 1;
-            //var b = 2;
-
-            //Console.WriteLine($"a:{a}");
-            //Console.WriteLine($"b:{b}");
-
-            Console.WriteLine($"{a.Name}:{a.Balance}");
-            Console.WriteLine($"{b.Name}:{b.Balance}");
-
-            Console.WriteLine();
             Console.ReadLine();
-        }
-
-        private static T Merge<T>(T a, T b) where T : IUser, new()
-        {
-            return new T()
-            {
-                Name = a.Name + b.Name,
-                Balance = a.Balance + b.Balance
-            };
-        }
-
-        private static void Swap(ref object a, ref object b)
-        {
-            var temp = b;
-            b = a;
-            a = temp;
-        }
-
-        private static void Swap<T>(T a, T b) where T : IUser
-        {
-            var temp = b.Balance;
-            b.Balance = a.Balance;
-            a.Balance = temp;
-        }
-
-        private static void Swap<T>(ref T a, ref T b)
-        {
-            var temp = b;
-            b = a;
-            a = temp;
-        }
-
-        private static void Swap(ref double b, ref double a)
-        {
-            var temp = b;
-            b = a;
-            a = temp;
-        }
-
-        private static void Swap(ref string b, ref string a)
-        {
-            var temp = b;
-            b = a;
-            a = temp;
         }
 
         private static void Swap(ref int b, ref int a)
@@ -96,23 +36,5 @@ namespace CSharpGeneric
             b = a;
             a = temp;
         }
-    }
-
-    internal class Customer : IUser
-    {
-        public string Name { get; set; }
-        public int Balance { get; set; }
-    }
-
-    internal interface IUser
-    {
-        string Name { get; set; }
-        int Balance { get; set; }
-    }
-
-    internal class Player : IUser
-    {
-        public string Name { get; set; }
-        public int Balance { get; set; }
     }
 }
